d0535e51469de437065a98d7cb5b844985363d81
Garrusir <garrusir@yandex.ru>
2019-06-15 20:57:55 +0300

    done

--- main.js
+++ main.js
@@ -1,7 +1,62 @@
 window.onload = () => {
-	btn = document.getElementById('nav-btn');
-	mobMenu = document.querySelector('.mob-nav');
+	let btn = document.getElementById('nav-btn');
+	let mobMenu = document.querySelector('.mob-nav');
 	btn.onclick = () => {
 		mobMenu.classList.toggle("active") ;
 	}
+
+	let slider = document.querySelector('header');
+	let dots = document.querySelectorAll(".slider-nav .item");
+	let currentSlide = 1;
+	for (item of dots){
+		item.onclick = (event) => {
+			currentSlide = event.target.id;
+			slide(currentSlide);
+			// event.target.classList.toggle('active');
+		}
+	}
+	document.querySelector(".left-arrow").onclick = () => {
+		if (currentSlide == 1) {
+			currentSlide = 3;
+			slide(currentSlide)
+		} else {
+			currentSlide--;
+			slide(currentSlide);
+		}
+	}
+
+	document.querySelector(".right-arrow").onclick = () => {
+		
+		if (currentSlide == 3) {
+			currentSlide = 1;
+			slide(currentSlide);
+		} else {
+			currentSlide++;
+			slide(currentSlide);
+		}
+	}
+	function slide(n) {
+		slider.classList.add('showing');
+		setTimeout(()=>{
+		slider.className = '';
+		slider.classList.add(`slide-${n}`);
+	}, 300)
+		
+		for (item of dots){
+			item.classList.remove('active');
+		}
+		console.log(dots[currentSlide-1]);
+		console.log(currentSlide);
+		dots[currentSlide-1].classList.add('active');
+	}
+
+	setInterval(()=>{
+		if (currentSlide == 3) {
+			currentSlide = 1;
+			slide(currentSlide);
+		} else {
+			currentSlide++;
+			slide(currentSlide);
+		}
+	}, 4000)
 }
